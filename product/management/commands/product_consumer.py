5a52c64a650f77a67c504e303989c4729ab32c58
chetan <chetan.fuloriya@agrevolution.in>
2025-08-25 17:16:02 +0530

    commneted extra code like rabbitmq and redis

--- product/management/commands/product_consumer.py
+++ product/management/commands/product_consumer.py
@@ -1,33 +1,33 @@
-from django.core.management.base import BaseCommand
-from kombu import Connection, Exchange, Queue, Consumer
-from product.models import Product
+# from django.core.management.base import BaseCommand
+# from kombu import Connection, Exchange, Queue, Consumer
+# from product.models import Product
 
 
-class Command(BaseCommand):
-    help = "Runs the RabbitMQ consumer for product creation events"
+# class Command(BaseCommand):
+#     help = "Runs the RabbitMQ consumer for product creation events"
 
-    def handle(self, *args, **options):
-        rabbit_url = "amqp://guest:guest@localhost:5672//"
-        connection = Connection(rabbit_url)
+#     def handle(self, *args, **options):
+#         rabbit_url = "amqp://guest:guest@localhost:5672//"
+#         connection = Connection(rabbit_url)
 
-        exchange = Exchange('product_creation_events', type='direct', durable=True)
-        queue = Queue(
-            'product_created_queue',
-            exchange,
-            routing_key='prdouct.create',   # careful, looks like typo
-            durable=True
-        )
+#         exchange = Exchange('product_creation_events', type='direct', durable=True)
+#         queue = Queue(
+#             'product_created_queue',
+#             exchange,
+#             routing_key='prdouct.create',   # careful, looks like typo
+#             durable=True
+#         )
 
-        def process_product_create_request(body, message):
-            self.stdout.write(f"Processing product: {body['product_name']}")
-            Product.objects.create(name=body['product_name'])
-            self.stdout.write(self.style.SUCCESS(f"Product {body['product_name']} created successfully."))
-            message.ack()
+#         def process_product_create_request(body, message):
+#             self.stdout.write(f"Processing product: {body['product_name']}")
+#             Product.objects.create(name=body['product_name'])
+#             self.stdout.write(self.style.SUCCESS(f"Product {body['product_name']} created successfully."))
+#             message.ack()
 
-        with connection:
-            with Consumer(connection, queues=queue, callbacks=[process_product_create_request], accept=['json']):
-                self.stdout.write(self.style.NOTICE("Waiting for messages. Press Ctrl+C to exit."))
-                while True:
-                    connection.drain_events()
+#         with connection:
+#             with Consumer(connection, queues=queue, callbacks=[process_product_create_request], accept=['json']):
+#                 self.stdout.write(self.style.NOTICE("Waiting for messages. Press Ctrl+C to exit."))
+#                 while True:
+#                     connection.drain_events()
 
-# This command can be run using `python manage.py product_consumer`
+# # This command can be run using `python manage.py product_consumer`
--- product/views.py
+++ product/views.py
@@ -7,7 +7,7 @@ import product
 from .models import Product
 import time
 from django.core.cache import cache
-from product.tasks import create_product, create_product_dead_letter_queue, create_product_high, create_product_low
+# from product.tasks import create_product, create_product_dead_letter_queue, create_product_high, create_product_low
 
 
 class ProductView(APIView):
@@ -21,56 +21,56 @@ class ProductView(APIView):
         return Response(data={"id": product.id, "name": product.name}, status=201)
 
 
-class ProductRedisView(APIView):
-    def post(self, request):
-        product_list = cache.get("product_list")
-        if product_list:
-            return Response(data=product_list, status=200)
-        time.sleep(5)
-        products =  Product.objects.all().values("id", "name")
-        cache.set("product_list", list(products), timeout=600)
-        return Response(data=products, status=200)
+# class ProductRedisView(APIView):
+#     def post(self, request):
+#         product_list = cache.get("product_list")
+#         if product_list:
+#             return Response(data=product_list, status=200)
+#         time.sleep(5)
+#         products =  Product.objects.all().values("id", "name")
+#         cache.set("product_list", list(products), timeout=600)
+#         return Response(data=products, status=200)
     
 
-class ProductCeleryView(APIView):
-    def post(self, request):
-        create_product.delay(8, 10)
-        # time.sleep(2)  # Simulate some processing time
-        create_product.delay(9, 3)
-        # time.sleep(2)
-        create_product.delay(10, 1)
-        # time.sleep(2)
-        create_product.delay(11, 1)
-        return Response(data={"message": "Product creation task has been queued."}, status=202)
+# class ProductCeleryView(APIView):
+#     def post(self, request):
+#         create_product.delay(8, 10)
+#         # time.sleep(2)  # Simulate some processing time
+#         create_product.delay(9, 3)
+#         # time.sleep(2)
+#         create_product.delay(10, 1)
+#         # time.sleep(2)
+#         create_product.delay(11, 1)
+#         return Response(data={"message": "Product creation task has been queued."}, status=202)
 
 
 
-class ProductMultipleCeleryView(APIView):
-    def post(self, request):
-        create_product_low.delay(1)
-        create_product_high.delay(1)
-        return Response(data={"message": "Multiple product creation tasks have been queued."}, status=202)
+# class ProductMultipleCeleryView(APIView):
+#     def post(self, request):
+#         create_product_low.delay(1)
+#         create_product_high.delay(1)
+#         return Response(data={"message": "Multiple product creation tasks have been queued."}, status=202)
 
 
-class ProductProducerSingleConsumer(APIView):
-    def post(self, request):
-        product_name = "Product_producer_single_consumer"
-        # Assuming you have a producer function to send the product creation event
-        from producer.product_creation_producer import send_product_creation_event
-        send_product_creation_event(product_name)
-        return Response(data={"message": f"Product creation event for {product_name} has been sent."}, status=202)
+# class ProductProducerSingleConsumer(APIView):
+#     def post(self, request):
+#         product_name = "Product_producer_single_consumer"
+#         # Assuming you have a producer function to send the product creation event
+#         from producer.product_creation_producer import send_product_creation_event
+#         send_product_creation_event(product_name)
+#         return Response(data={"message": f"Product creation event for {product_name} has been sent."}, status=202)
 
 
-class ProductProducerSingleConsumerPersisitentConnection(APIView):
-    def post(self, request):
-        product_name = "Product_producer_single_consumer_persistnet_connection"
-        # Assuming you have a producer function to send the product creation event
-        from producer.product_creation_producer import send_product_creation_event
-        rabbitmq_publisher.publish_order_event(product_name)
-        return Response(data={"message": f"Product creation event for {product_name} has been sent."}, status=202)
+# class ProductProducerSingleConsumerPersisitentConnection(APIView):
+#     def post(self, request):
+#         product_name = "Product_producer_single_consumer_persistnet_connection"
+#         # Assuming you have a producer function to send the product creation event
+#         from producer.product_creation_producer import send_product_creation_event
+#         rabbitmq_publisher.publish_order_event(product_name)
+#         return Response(data={"message": f"Product creation event for {product_name} has been sent."}, status=202)
 
 
-class ProductDeadLetterView(APIView):
-    def post(self, request):
-        create_product_dead_letter_queue.delay(11)
-        return Response(data={"message": "product creation tasks have been queued and exception sendd to dead letter queue."}, status=202)
+# class ProductDeadLetterView(APIView):
+#     def post(self, request):
+#         create_product_dead_letter_queue.delay(11)
+#         return Response(data={"message": "product creation tasks have been queued and exception sendd to dead letter queue."}, status=202)
